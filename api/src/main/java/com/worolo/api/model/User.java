e6fdbe1c69dda48f71584c155c5f8646a0e7bfc3
sergeemmanuelk <serge_em@outlook.fr>
2023-03-13 14:37:08 +0000

    Add Getter/Setter, Constructor

--- api/src/main/java/com/worolo/api/model/User.java
+++ api/src/main/java/com/worolo/api/model/User.java
@@ -9,9 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
 
-@Data
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -37,4 +36,88 @@ public class User {
 	
 	@OneToMany(mappedBy = "user")
 	private List<Certificate> certificate;
+	
+
+	public User() {
+		super();
+	}
+
+	public User(Long id, char gender, String firstName, String lastName, String email, String password, int phoneNumber,
+			String address) {
+		super();
+		this.id = id;
+		this.gender = gender;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 }
