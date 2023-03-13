53e655a0e20fb9ad12972fa716b3ad02f7a25779
sergeemmanuelk <serge_em@outlook.fr>
2023-03-13 16:41:16 +0000

    add RestController and RequestMapping annotation

--- api/src/main/java/com/worolo/api/controller/HealthCenterController.java
+++ api/src/main/java/com/worolo/api/controller/HealthCenterController.java
@@ -9,10 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.worolo.api.model.HealthCenter;
 import com.worolo.api.service.HealthCenterService;
 
+@RestController
+@RequestMapping("/api/healthcenter")
 public class HealthCenterController {
 	@Autowired
 	private HealthCenterService hcService;
