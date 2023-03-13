6efe2822f4fc9ca63ddab5e6d2bce5d8825a31df
sergeemmanuelk <serge_em@outlook.fr>
2023-03-13 16:04:04 +0000

    Create a MunicipalityService class that implements CRUD operations

---
+++ api/src/main/java/com/worolo/api/service/MunicipalityService.java
@@ -0,0 +1,50 @@
+package com.worolo.api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.worolo.api.model.HealthCenter;
+import com.worolo.api.model.Municipality;
+import com.worolo.api.repository.MunicipalityRepository;
+
+@Service
+public class MunicipalityService {
+	@Autowired
+	private MunicipalityRepository municipalityRepository;
+	
+	public List<Municipality> getAllMunicipality() {
+		return municipalityRepository.findAll();
+	}
+	
+	public Municipality getMunicipalityById(Long id) {
+		return municipalityRepository.findById(id).orElse(null);
+	}
+	
+	public Municipality createMunicipality(Municipality municipality) {
+		return municipalityRepository.save(municipality);
+	}
+	
+	public Municipality updateMunicipality(Long id, HealthCenter updatedMunicipality) {
+		Municipality existingMunicipality = municipalityRepository.findById(id).orElse(null);
+        if (existingMunicipality != null) {
+        	existingMunicipality.setName(updatedMunicipality.getName());
+        	existingMunicipality.setPhoneNumber(updatedMunicipality.getPhoneNumber());
+        	existingMunicipality.setAddress(updatedMunicipality.getAddress());
+        	existingMunicipality.setEmail(updatedMunicipality.getEmail());
+            
+            return municipalityRepository.save(existingMunicipality);
+        }
+        return null;
+	}
+	
+	public boolean deleteHealthCenter(Long id) {
+		Municipality existingMunicipality = municipalityRepository.findById(id).orElse(null);
+        if (existingMunicipality != null) {
+        	municipalityRepository.delete(existingMunicipality);
+            return true;
+        }
+        return false;
+	}
+}
