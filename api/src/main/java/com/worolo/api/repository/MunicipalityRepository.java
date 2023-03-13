6045bf7194abed33d26e3d3f981a4368a0493686
sergeemmanuelk <serge_em@outlook.fr>
2023-03-13 15:46:07 +0000

    Creating MunicipalityRepository interface

--- api/src/main/java/com/worolo/api/repository/MunicipalityRepository.java
+++ api/src/main/java/com/worolo/api/repository/MunicipalityRepository.java
@@ -1,9 +1,11 @@
 package com.worolo.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.worolo.api.model.Municipality;
 
+@Repository
 public interface MunicipalityRepository extends JpaRepository<Municipality, Long> {
 
 }
