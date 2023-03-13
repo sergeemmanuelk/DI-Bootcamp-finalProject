665dd6b43f7fa9c3014815e735c51927f7b50f9a
sergeemmanuelk <serge_em@outlook.fr>
2023-03-13 14:54:44 +0000

    feat: Create a UserController class that handles HTTP requests and responses

---
+++ api/src/main/java/com/worolo/api/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.worolo.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.worolo.api.model.User;
+import com.worolo.api.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping
+	public List<User> getAllUser() {
+		return userService.getAllUser();
+	}
+	
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable Long id) {
+		return userService.getUserById(id);
+	}
+	
+	@PostMapping
+	public User createUser(@RequestBody User user) {
+		return userService.createUser(user);
+	}
+	
+	@PutMapping("/{id}")
+	public User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+		return userService.updateUser(id, updatedUser);
+	}
+	
+	@DeleteMapping("/{id}")
+	public boolean deleteUser(@PathVariable Long id) {
+		return userService.deleteUser(id);
+	}
+}
