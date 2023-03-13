f78c525590be118a9c46999553ae0e26e96b32fd
sergeemmanuelk <serge_em@outlook.fr>
2023-03-13 14:58:29 +0000

    Add Getter/Setter and Constructor

--- api/src/main/java/com/worolo/api/model/HealthCenter.java
+++ api/src/main/java/com/worolo/api/model/HealthCenter.java
@@ -6,9 +6,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
 
-@Data
+
 @Entity
 @Table(name = "health_centers")
 public class HealthCenter {
@@ -26,5 +25,60 @@ public class HealthCenter {
 	
 	@ManyToOne
 	private Municipality municipality;
+
+	public HealthCenter() {
+		super();
+	}
+
+	public HealthCenter(Long id, String name, String email, int phoneNumber, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 
 }
