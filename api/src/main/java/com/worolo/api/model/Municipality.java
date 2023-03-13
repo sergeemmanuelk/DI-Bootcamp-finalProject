46d7063d981015b185b96517930d443262f6e57c
sergeemmanuelk <serge_em@outlook.fr>
2023-03-13 15:52:01 +0000

    Add Getter/Setter and Constructor

--- api/src/main/java/com/worolo/api/model/Municipality.java
+++ api/src/main/java/com/worolo/api/model/Municipality.java
@@ -31,4 +31,59 @@ public class Municipality {
 	
 	@OneToMany(mappedBy = "municipality")
 	private List<Certificate> certificate;
+
+	public Municipality() {
+		super();
+	}
+
+	public Municipality(Long id, String name, String email, int phoneNumber, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 }
