4eedb6116ac9626e0282c97b133b1264ada06f5f
sergeemmanuelk <serge_em@outlook.fr>
2023-03-17 14:46:18 +0000

    Create a CitizenService class that implements CRUD operations

---
+++ api/src/main/java/com/worolo/api/service/CitizenService.java
@@ -0,0 +1,54 @@
+package com.worolo.api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.worolo.api.model.Citizen;
+import com.worolo.api.model.Person;
+import com.worolo.api.repository.CitizenRepository;
+
+@Service
+public class CitizenService {
+	@Autowired
+	private CitizenRepository citizenRepository;
+	
+	public List<Citizen> getAllCItizen() {
+		return citizenRepository.findAll();
+	}
+	
+	public Citizen getCitizenById(Long id) {
+		return citizenRepository.findById(id).orElse(null);
+	}
+	
+	public Citizen createCitizen(Citizen citizen) {
+		return citizenRepository.save(citizen);
+	}
+	
+	public Citizen updateCitizen(Long id, Citizen updatedCitizen) {
+		Citizen existingCitizen = citizenRepository.findById(id).orElse(null);
+        if (existingCitizen != null) {
+        	existingCitizen.setGender(updatedCitizen.getGender());
+        	existingCitizen.setFirstName(updatedCitizen.getFirstName());
+        	existingCitizen.setLastName(updatedCitizen.getLastName());
+        	existingCitizen.setEmail(updatedCitizen.getEmail());
+        	existingCitizen.setPassword(updatedCitizen.getPassword());
+        	existingCitizen.setBirthdate(updatedCitizen.getBirthdate());
+        	existingCitizen.setPhoneNumber(updatedCitizen.getPhoneNumber());
+        	existingCitizen.setAddress(updatedCitizen.getAddress());
+            
+            return citizenRepository.save(existingCitizen);
+        }
+        return null;
+	}
+	
+	public boolean deleteCitizen(Long id) {
+		Citizen existingCitizen = citizenRepository.findById(id).orElse(null);
+        if (existingCitizen != null) {
+        	citizenRepository.delete(existingCitizen);
+            return true;
+        }
+        return false;
+	}
+}
