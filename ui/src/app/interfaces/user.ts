9bed1f6690e8cf1e238ef0aeda137d28b220273e
sergeemmanuelk <serge_em@outlook.fr>
2023-03-17 16:37:28 +0000

    Rename IUser to IPerson and update interface

--- ui/src/app/interfaces/user.ts
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-    gender : string;
-    firstName : string;
-    lastName : string;
-    email : string ;
-    password : string;
-	phoneNumber : number;
-	address : string;
-}
