b844fc9d860a6d27611cdd987e44bd0720eb88ac
sergeemmanuelk <serge_em@outlook.fr>
2023-03-17 10:29:49 +0000

    refactor: remplace Citizen model to Person model

--- api/src/main/java/com/worolo/api/repository/PersonRepository.java
+++ api/src/main/java/com/worolo/api/repository/PersonRepository.java
@@ -3,9 +3,9 @@ package com.worolo.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.worolo.api.model.Citizen;
+import com.worolo.api.model.Person;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Citizen, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 
 }
