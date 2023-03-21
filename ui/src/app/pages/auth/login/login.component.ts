16eaa6aee544f382835a4c54a51370300b10d151
sergeemmanuelk <serge_em@outlook.fr>
2023-03-21 09:54:50 +0000

    feat: make login page for auth

--- ui/src/app/pages/auth/login/login.component.ts
+++ ui/src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IPerson } from 'src/app/interfaces/person';
+import { PersonService } from 'src/app/services/storage/person.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  error : string = '';
+  person : IPerson = {
+    email : '',
+    password : ''
+  }
 
+  constructor(private PersonService : PersonService , private router : Router) {
+
+  }
+
+  login(){
+    const person = this.PersonService.getPerson();
+
+    if(person?.email == this.person.email && person?.password == this?.person.password) {
+      return location.href = "/home";
+    }
+
+    return (this.error = "Email ou mot de passe incorrect !")
+  }
 }
